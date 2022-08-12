163d874810534071f1c09c642068ca948c8d1192
Caleb <Tigers1025@gmail.com>
2022-08-12 11:32:26 -0400

    commit tree 6ecf055c2e875edeee4554d839821b37b7587d04

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(array){
+  const newObj = array.find(array=>array.result === 'W')
+  return newObj ? newObj.year : undefined
+     
+       
+
+}
+
